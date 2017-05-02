becdad74585f1a4bfb670bfd3c3cb35be84a3e71
hoonga <etaphirho@snu.ac.kr>
2017-05-02 21:53:10 +0900

    not tested but somewhat done

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 var express = require('express');
-var db = require('./db');
-db.connect('test');
-var mongoose = db.mongoose
-var url = db.test;
-var app = express();
 var bodyParser = require('body-parser');
-var question = require('./api/question');
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server)
+
+var api = {}; api.question = require('./api/question');
+var socket = {}; socket.question = require('./socket/question');
 
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/api/question', question);
+
+/*io.on('connect', (socket) => {
+    console.log('socket connected');
+});*/
+var questionIO = io.of('/socket/question');
+app.use('/api/question', (req, res, next) => {
+    req.io = questionIO;
+    next();
+});
+//quiestionIO.on
+app.use('/api/question', api.question);
+app.use('/api/question', socket.question);
+
 app.use('/public', express.static('public'));
 app.use('/', express.static('view'));
 
-var server = app.listen(8000, function (){
-    console.log("open at port 8000");
+//var db = require('./db');
+//db.connect('test');
+var port = 8000;
+server.listen(port, () => {
+    console.log('listening on port {$port}');
 });
+module.exports = app;
