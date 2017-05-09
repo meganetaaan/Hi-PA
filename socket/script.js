118f8ab4149dbc52e9c3b756bedc4b9b00f9fd35
hoonga <etaphirho@snu.ac.kr>
2017-05-09 19:59:02 +0900

    added script recv, send sock

---
+++ socket/script.js
@@ -0,0 +1,22 @@
+var Script = require('../model/Script');
+
+module.exports = function (io) {
+    var script = {};
+    script.audience = io.of('/socket/audience');
+    script.presenter = io.of('/socket/presenter');
+    script.presenter.on('ADD_SCRIPT', function f(data) {
+        /*if (typeof f.cached == 'undefined') {
+            f.cached = new Script(data);
+        }
+        if (f.cached.*/
+        var s = new Script(data);
+        s.save((err, res) => {
+            if(err)
+                return;
+            else
+                script.audience.emit('ADD_SCRIPT', data);
+        });
+    });
+
+    return script;
+}
