064a6ee7e77b40890c24f26f05b78fc5280815da
hoonga <etaphirho@snu.ac.kr>
2017-05-09 23:16:34 +0900

    again typo fix

--- socket/script.js
+++ socket/script.js
@@ -5,21 +5,21 @@ module.exports = function (io) {
     script.audience = io.of('/socket/audience');
     script.presenter = io.of('/socket/presenter');
     script.presenter.on('connection', (socket) => {
-      console.log('connected');
-      socket.on('ADD_SCRIPT', function f(socket, data) {
-        console.log(data);
-        /*if (typeof f.cached == 'undefined') {
-            f.cached = new Script(data);
-        }
-        if (f.cached.*/
-        var s = new Script(data);
-        s.save((err, res) => {
-            if(err)
-                return;
-            else
-                script.audience.emit('ADD_SCRIPT', data);
+        console.log('connected');
+        socket.on('ADD_SCRIPT', function f(socket, data) {
+            console.log(data);
+            /*if (typeof f.cached == 'undefined') {
+                f.cached = new Script(data);
+            }
+            if (f.cached.*/
+            var s = new Script(data);
+            s.save((err, res) => {
+                if(err)
+                    return;
+                else
+                    script.audience.emit('ADD_SCRIPT', data);
+            });
         });
-      });
-    }
+    });
     return script;
 }
