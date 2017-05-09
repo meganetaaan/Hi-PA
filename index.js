7e48cb8304efb6f4c5c818acd9ee0d00bd620528
hoonga <etaphirho@snu.ac.kr>
2017-05-09 22:54:33 +0900

    added https

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ var sassMiddleware = require('node-sass-middleware')
 var bodyParser = require('body-parser');
 
 var app = express();
-var server = require('http').Server(app);
+var server;
+//https
+if (process.env.HTTPS == 'true') {
+    var fs = require('fs');
+    var options = {
+        key : fs.readFileSync('/etc/letsencrypt/live/hoonga.kr/privkey.pem'),
+        cert : fs.readFileSync('/etc/letsencrypt/live/hoonga.kr/cert.pem')
+    }
+    server = require('https').Server(options, app);
+}
+else
+    server = require('http').Server(app);
 var io = require('socket.io')(server)
 
 var api = {}; api.question = require('./api/question');
@@ -24,7 +35,6 @@ app.use('/api/question', (req, res, next) => {
 app.use('/api/question', api.question);
 app.use('/api/question', socket.question);
 
-
 app.use('/public/stylesheets',
     sassMiddleware({
         src: __dirname + '/public/stylesheets/sass', //where the sass files are
@@ -35,6 +45,7 @@ app.use('/public/stylesheets',
 app.use('/public', express.static('public'));
 
 app.get('/presenter', (req, res) => {
+    console.log('presenter connected');
     res.render('index', {
         isPresenter: true,
     });
