e64a1a45460ab74a60bc532d23f071e90da62a26
hoonga <etaphirho@snu.ac.kr>
2017-05-16 22:17:35 +0900

    audience initdata added

--- socket/slide.js
+++ socket/slide.js
@@ -2,6 +2,17 @@ var slideCtrl = require('../slide.controller');
 module.exports = function (io) {
     var slide = {};
     slide.audience = io.of('/socket/slide/audience');
+    slide.audience.on('connection', function(socket) {
+        console.log('client connected to slides');
+        socket.on('disconnect', function(socket) {
+            console.log('client disconnected out slides');
+        });
+        let slides = slideCtrl.getSlide('slide_01');
+        let initData = {
+            slideData : slides
+        }
+        socket.emit('initdata', initData);
+    });
     slide.presenter = io.of('/socket/slide/presenter');
     slide.presenter.on('connection', function(socket) {
         console.log('presenter connected to slides');
@@ -10,9 +21,9 @@ module.exports = function (io) {
         });
 
         // Get Init Data
-        var slides = slideCtrl.getSlide('slide_01');
-        var initData = {
-            slideData : slides,
+        let slides = slideCtrl.getSlide('slide_01');
+        let initData = {
+            slideData : slides
         }
         socket.emit('initdata', initData);
 
