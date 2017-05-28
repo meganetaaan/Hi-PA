727678c4c53f940fc937d01db4c7dfb2e9f1bee1
rajin9601 <rajin9601@naver.com>
2017-05-28 21:45:18 +0900

    Feedback ui for Presenter

--- public/javascripts/controller/FeedbackController.js
+++ public/javascripts/controller/FeedbackController.js
@@ -1,12 +1,17 @@
 var controller = {
     __name: 'hipa.controller.FeedbackController',
 
-    __templates: ['public/views/feedback.ejs'],
+    __templates: ['public/views/feedbackForm.ejs','public/views/feedbackShow.ejs'],
 
     socket: null,
-
+    _data: {fast: 0, slow: 0, loud: 0, small: 0},
+    _selectedFeedback: {speed: 0, volume: 0},
     __init: function(context) {
-      this.view.update('{rootElement}','feedback');
+      if (config.isPresenter) {
+        this.view.update('{rootElement}','feedbackShow');
+      } else {
+        this.view.update('{rootElement}','feedbackForm', this._selectedFeedback);
+      }
       this._setSocket();
     },
 
@@ -14,17 +19,51 @@ var controller = {
       if (config.isPresenter) {
         this.socket = io('/socket/feedback/presenter');
         // TODO: Feedback ui for presenter
+        this.socket.on('SpeedFeedback', (data) => {
+          console.log(data);
+          if (this._data.fast !== data.fast) {
+            this._refreshShow('#feedback-fast-num', data.fast);
+          }
+          if (this._data.slow !== data.slow) {
+            this._refreshShow('#feedback-slow-num', data.slow);
+          }
+          this._data.fast = data.fast;
+          this._data.slow = data.slow;
+        });
+        this.socket.on('VolumeFeedback', (data) => {
+          if (this._data.loud !== data.loud) {
+            this._refreshShow('#feedback-loud-num', data.loud);
+          }
+          if (this._data.small !== data.small) {
+            this._refreshShow('#feedback-small-num', data.small);
+          }
+          this._data.loud = data.loud;
+          this._data.small = data.small;
+        });
       } else {
         this.socket = io('/socket/feedback/audience');
       }
     },
 
+    _refreshShow(id, text) {
+      let $e = $(id);
+      $e.addClass('is-focus');
+      setTimeout(function() {
+        $e.removeClass('is-focus');
+      }, 100);
+      $e.find('span').text(text);
+    },
+
     _speedFeedback(sign) {
       this.socket.emit('SpeedFeedback', {sign: sign});
+      this._selectedFeedback.speed = sign;
+      this.view.update('{rootElement}', 'feedbackForm', this._selectedFeedback);
     },
 
     _volumeFeedback(sign) {
       this.socket.emit('VolumeFeedback', {sign: sign});
+      this._selectedFeedback.volume = sign;
+      this.view.update('{rootElement}', 'feedbackForm', this._selectedFeedback);
     },
 
     '.feedback-volume click': function(context, $el) {
