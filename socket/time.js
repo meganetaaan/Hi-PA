57412cccd27e644ec9c408d11fd3432c46bd088d
hoonga <etaphirho@snu.ac.kr>
2017-06-02 17:31:48 +0900

    need time added

--- socket/time.js
+++ socket/time.js
@@ -2,6 +2,7 @@ var io = require('./io').io();
 var Time = require('../model/time');
 var Script = require('../model/Script');
 var Question = require('../model/Question');
+var question = require('./question');
 
 var time = {};
 time.presenter = io.of('/socket/time/presenter');
@@ -11,6 +12,7 @@ time.presenter.on('connection', (socket) => {
         if (data.state == 'END') {
             Script.reset();
             Question.reset();
+            question.io.emit('DELETE_ALL', {});
         }
         Time.setTimeState(data);
     });
