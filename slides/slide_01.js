44be3f24946ab0ddc849b3011b677fce99b35eaf
rajin9601 <rajin9601@naver.com>
2017-06-23 18:37:11 +0900

    slide & slide meta-data

--- slides/slide_01.js
+++ slides/slide_01.js
@@ -4,6 +4,6 @@ var jsonPath = path.join(__dirname, 'slide_01.html');
 module.exports = {
     slideName : "slide_1",
     content : fs.readFileSync(jsonPath,'utf8'),
-    slideNo : 7,
-    duration : 210
+    slideNo : 50,
+    duration : 2100,
 }
